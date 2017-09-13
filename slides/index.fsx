1eeaa9b5f316674e9f93c7e5738fa215d7cb9539
Ryan Riley <ryanriley@live.com>
2017-09-13 08:44:28 -0500

    Add basic slide content

--- slides/index.fsx
+++ slides/index.fsx
@@ -7,6 +7,70 @@
 
 ***
 
-# The Agony and the Ecstasy of F# Scripting
+## The Agony and the Ecstasy of F# Scripting
+
+***
+
+## Use Cases
+
+---
+
+### Design
+
+---
+
+### Testing
+
+---
+
+### Communicating
+
+1. Documentation
+2. Slide decks
+3. Literate programming
+
+---
+
+### Build and Deploy
+
+---
+
+### Configuration
+
+---
+
+### Runtime!
+
+1. Azure Functions
+2. AWS Lambda
+3. etc.
+
+***
+
+## Common Problems
+
+---
+
+### References
+
+---
+
+### Current Directory
+
+---
+
+### Accessing App.config
+
+---
+
+### Compiled or Interactive
+
+#### Why choose?
+
+' Trying to run code in F# interactive that has been written
+' as a compiled library or executable can sometimes be tricky.
+' We'll look at techniques to make this less difficult.
+
+***
 
 *)
