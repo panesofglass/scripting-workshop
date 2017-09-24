c23b8035b60ddd956dcf0d059b4ae21ba0a5b3a6
Ryan Riley <ryanriley@live.com>
2017-09-24 16:09:43 -0500

    Combination exercise

--- completed/AppSettings.fsx
+++ completed/AppSettings.fsx
@@ -1,34 +1,11 @@
-#r "System.Configuration.dll"
 #load "../.paket/load/net462/Completed/completed.group.fsx"
-
-let [<Literal>] appSettingsPath = __SOURCE_DIRECTORY__ + "/AppSettings.fsx.config"
 
 (**
  * Option 1: Use System.Xml.Linq 
  *)
 
-#r "System.Xml.dll"
-#r "System.Xml.Linq.dll"
-open System.Xml
-open System.Xml.Linq
-
-#if INTERACTIVE
-type ConfigurationManager() =
-    static let config = XDocument.Load appSettingsPath
-    static let section (config:XDocument) name key value =
-        query {
-            for els in config.Descendants(XName.Get name) do
-            for el in els.Descendants(XName.Get "add") do
-            let k = el.Attribute(XName.Get key).Value 
-            let v = el.Attribute(XName.Get value).Value 
-            select (k,v)
-        }
-        |> dict
-    static let appSettings = section config "appSettings" "key" "value"
-    static let connectionStrings = section config "connectionStrings" "name" "connectionString"
-    static member AppSettings = appSettings
-    static member ConnectionStrings = connectionStrings
-#endif
+#load "Configuration.fsx"
+open Configuration
 
 ConfigurationManager.AppSettings.["TestInt"]
 ConfigurationManager.ConnectionStrings.["Test1"]
@@ -37,11 +14,12 @@ ConfigurationManager.ConnectionStrings.["Test1"]
  * Option 2: Use the FSharp.Configuration Type Provider
  *)
 
+#r "System.Configuration.dll"
 open FSharp.Configuration
 
-let [<Literal>] appSettingsPath = __SOURCE_DIRECTORY__ + "/AppSettings.fsx.config"
-type Settings = FSharp.Configuration.AppSettings<appSettingsPath>
-let [<Literal>] executableFile = __SOURCE_DIRECTORY__ + "/AppSettings.fsx"
-Settings.SelectExecutableFile executableFile
+let [<Literal>] AppSettingsPath = __SOURCE_DIRECTORY__ + "/AppSettings.fsx.config"
+type Settings = FSharp.Configuration.AppSettings<AppSettingsPath>
+let [<Literal>] ExecutableFile = __SOURCE_DIRECTORY__ + "/AppSettings.fsx"
+Settings.SelectExecutableFile ExecutableFile
 
 Settings.TestUri
